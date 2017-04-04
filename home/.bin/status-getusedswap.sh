985de2d4283fa790ba4e4d368c1ba33f08e4f5bc
nalabelle <nalabelle@gmail.com>
2017-04-04 04:19:31 +0000

    some systems don't have swap

--- home/.bin/status-getusedswap.sh
+++ home/.bin/status-getusedswap.sh
@@ -2,5 +2,5 @@
 if [[ $OSTYPE == "darwin"* ]]; then
   sysctl vm.swapusage | awk '{print $4, $7, $10}' | sed 's/M//g' | awk '{ printf "%.0f\n",(($2/$1)*100) }'
 else
-  awk '/SwapTotal:/{total=$2} /SwapFree:/{free=$2} END { printf "%.0f\n",((total-free)*100/total) }' /proc/meminfo
+  awk '/SwapTotal:/{total=$2} /SwapFree:/{free=$2} END { if (total > 0) { printf "%.0f\n",((total-free)*100/total) } else { printf "-\n" } }' /proc/meminfo
 fi
