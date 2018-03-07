bbba21e98cb54db960eea6c21733a791c43a4734
nalabelle <git@nalabelle.com>
2018-03-07 13:24:45 -0800

    update setup script

--- setup.sh
+++ setup.sh
@@ -13,16 +13,13 @@
 ### Install homeshick ###
 if [ ! -d $HOME/.homesick/repos/homeshick ]; then
   git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-  source $HOME/.homesick/repos/homeshick/homeshick.sh
+  . $HOME/.homesick/repos/homeshick/homeshick.sh
 else
-  source $HOME/.homesick/repos/homeshick/homeshick.sh
+  . $HOME/.homesick/repos/homeshick/homeshick.sh
 fi
 
 if [ ! -d $HOME/.homesick/repos/dotfiles ]; then
-  homeshick clone git@github.com:nalabelle/dotfiles.git
-fi
-if [ ! -d $HOME/.homesick/repos/binfiles ]; then
-  homeshick clone git@github.com:nalabelle/binfiles.git
+  homeshick clone git://github.com/nalabelle/dotfiles.git
 fi
 
 # Install Vundle
@@ -34,14 +31,5 @@ fi
 if [ ! -d $HOME/.tmux/plugins/tpm ]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
-
-### Link it all to $HOME ###
-echo "Force link all files?"
-select yn in "Yes" "No"; do
-  case $yn in
-    Yes ) $homeshick link --force; break;;
-    No  ) break;;
-  esac
-done
 
 echo "Completed"
