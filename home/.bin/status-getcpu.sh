03a86e38d8f144369f86115feec100c1f6de0899
nalabelle <git@nalabelle.com>
2018-08-22 15:44:21 -0700

    trying a more portable method of getting cores

--- home/.bin/status-getcpu.sh
+++ home/.bin/status-getcpu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-if [[ $OSTYPE == "darwin"* ]]; then
-  sysctl -n hw.ncpu
-else
-  grep -c ^processor /proc/cpuinfo
-fi
+getconf _NPROCESSORS_ONLN
+
+#if [[ $OSTYPE == "darwin"* ]]; then
+#  sysctl -n hw.ncpu
+#else
+#  grep -c ^processor /proc/cpuinfo
+#fi
