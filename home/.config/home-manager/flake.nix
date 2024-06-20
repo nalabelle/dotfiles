f84abaf315a15438ddf27d8586b05a6238f30a8d
nalabelle <git@nalabelle.com>
2024-06-20 15:50:11 -0700

    multiarch home-manager

--- home/.config/home-manager/flake.nix
+++ home/.config/home-manager/flake.nix
@@ -9,15 +9,30 @@
     };
   };
 
-  outputs = { self, home-manager, nixpkgs }: {
-    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-
-    homeConfigurations = {
-      nalabelle = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-        modules = [ ./home.nix ];
+  outputs = { self, home-manager, nixpkgs }:
+    let
+      config = {
+        x86_64-linux = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+          modules = [
+            ./home.nix
+            {
+              home.homeDirectory = "/home/nalabelle";
+            }
+          ];
+        };
+        aarch64-darwin = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs { system = "aarch64-darwin"; };
+          modules = [
+            ./home.nix
+            {
+              home.homeDirectory = "/Users/nalabelle";
+            }
+          ];
+        };
       };
+    in {
+      packages.aarch64-darwin.homeConfigurations.nalabelle = config.aarch64-darwin;
+      packages.x86_64-linux.homeConfigurations.nalabelle = config.x86_64-linux;
     };
-  };
 }
-
