3a997f331fc61dcdcbe95f8a67ccb920e2f1825a
nalabelle <git@nalabelle.com>
2024-12-04 16:51:58 -0800

    feat: remove some unused scripts

--- bin/status-getusedmem.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ $OSTYPE == "darwin"* ]]; then
-  vm_stat | awk '/Pages free/{free=$3} /Pages active/{active=$3} /Pages inactive/{inactive=$3}/Pages speculative/{speculative=$3} /Pages wired down/{wired=$4} END { printf "%.0f\n", ((active + inactive + wired)/(free + active + inactive + speculative + wired)*100) }'
-else
-  awk '/MemTotal:/{total=$2} /MemFree:/{free=$2} END { printf "%.0f\n",((total-free)*100/total) }' /proc/meminfo
-fi
--- bin/status-getusedswap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ $OSTYPE == "darwin"* ]]; then
-  sysctl vm.swapusage | awk '{print $4, $7, $10}' | sed 's/M//g' | awk '{ printf "%.0f\n",(($2/$1)*100) }'
-else
-  awk '/SwapTotal:/{total=$2} /SwapFree:/{free=$2} END { if (total > 0) { printf "%.0f\n",((total-free)*100/total) } else { printf "-\n" } }' /proc/meminfo
-fi
