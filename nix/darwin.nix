4cfedc325b824b8c9d1fbeadb4d5b3ebd37bebec
nalabelle <2044448+nalabelle@users.noreply.github.com>
2025-08-04 10:03:33 -0700

    refactor: update activation script and disable desktop click-to-show feature

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -72,14 +72,16 @@
 
       # activateSettings -u will reload the settings from the database and apply them to the current session,
       # so we do not need to logout and login again to make the changes take effect.
-      echo "Activating system settings..."
-      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+      #echo "Activating system settings..."
+      #/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
     '';
 
     defaults = {
       smb.NetBIOSName = hostname;
       menuExtraClock.Show24Hour = true;
       menuExtraClock.ShowAMPM = false;
+      # Disable "windows move aside when clicking on desktop" feature
+      WindowManager.EnableStandardClickToShowDesktop = false;
       dock = {
         show-recents = false;
         autohide = true;
